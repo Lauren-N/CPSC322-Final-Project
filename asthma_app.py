2fa93f50431daaf559f6be92792994cf7377d0e3
laurenchin8 <123130930+laurenchin8@users.noreply.github.com>
2024-12-11 04:05:57 +0000

    working on flask app with random forest predictor

--- asthma_app.py
+++ asthma_app.py
@@ -1,9 +1,12 @@
 import os
 import pickle
+import myutils
 
 from flask import Flask
 from flask import render_template
 from flask import request, jsonify, redirect
+from classifier import MyDecisionTreeClassifier
+from collections import Counter
 
 app = Flask(__name__)
 
@@ -11,29 +14,16 @@ app = Flask(__name__)
 def index_page():
     prediction = ""
     if request.method == "POST":
-        level = request.form.get("level")
-        lang = request.form.get("lang")
-        tweets = request.form.get("tweets")
-        phd = request.form.get("phd")
-        prediction = predict_interviews_well([level, lang, tweets, phd])
+        bmi = request.form.get("BMI")
+        smoking = request.form.get("Smoking")
+        pa = request.form.get("PhysicalActivity")
+        ev1 = request.form.get("LungFunctionFEV1")
+
+        instance = [bmi, smoking, pa, ev1]
+        prediction = predict_asthma.predict(instance)
     print("prediction:", prediction)
     # goes into templates folder and finds given name
     return render_template("index.html", prediction=prediction) 
-
-# @app.route('/', methods = ['GET', 'POST'])
-# def index_page():
-#     prediction = ""
-#     if request.method == "POST":
-#         bmi = request.form.get("BMI")
-#         smoking = request.form.get("Smoking")
-#         pa = request.form.get("PhysicalActivity")
-#         ev1 = request.form.get("LungFunctionFEV1")
-
-#         instance = [bmi, smoking, pa, ev1]
-#         prediction = my_random_forest.predict(instance)
-#     print("prediction:", prediction)
-#     # goes into templates folder and finds given name
-#     return render_template("index.html", prediction=prediction) 
 
 # lets add a route for the /predict endpoint
 @app.route("/predict")
@@ -47,13 +37,13 @@ def predict():
     
     instance = [bmi, smoking, pa, ev1]
     # lets make a prediction!
-    pred = predict_interviews_well.predict(instance)
+    pred = predict_asthma.predict(instance)
     if pred is not None:
         return jsonify({"prediction": pred}), 200
     # something went wrong!!
     return "Error making a prediction", 400
 
-# recursive
+# classifier
 def tdidt_classifier(tree, header, instance):
     info_type = tree[0]
     if info_type == "Attribute":
@@ -67,13 +57,29 @@ def tdidt_classifier(tree, header, instance):
         leaf_label = tree[1]
         return leaf_label
 
-def predict_interviews_well(unseen_instance):
+# classifier
+def rf_classifier(trees, header, instance):
+    # Collect predictions from all trees
+    all_preds = []
+    
+    for tree in trees:
+        # Use the tdidt_classifier to predict from each tree
+        prediction = tdidt_classifier(tree, header, instance)
+        all_preds.append(prediction)
+    
+    # Majority vote for each instance
+    votes = Counter(all_preds)
+    majority_vote = votes.most_common(1)[0][0]  # Get the most common prediction
+    
+    return majority_vote
+
+def predict_asthma(unseen_instance):
     # deserialize to object (unpickle)
     infile = open("tree.p", "rb")
-    header, interview_tree = pickle.load(infile)
+    header, asthma_trees = pickle.load(infile)
     infile.close()
     try:
-        return tdidt_classifier(interview_tree, header, unseen_instance)
+        return rf_classifier(asthma_trees, header, unseen_instance)
     except:
         return None
 
--- tree_pickler.py
+++ tree_pickler.py
@@ -1,7 +1,8 @@
 import pickle # standard library
+import myutils
+import mypytable
+import classifier
 
-# asthma_header = ["BMI", "Smoking", "PhysicalActivity", "LungFunctionFEV1"]
-# asthma_tree = tree
 
 # pickling is used for object serialization and deserialization
 # pickle: write a binary representation of an object to a file (for use later...)
@@ -40,8 +41,55 @@ interview_tree_solution =   ["Attribute", "level",
                                 ]
                             ]
 
+asthma_header = ["BMI", "Smoking", "PhysicalActivity", "LungFunctionFEV1"]
+
+# Now to generate asthma_trees
+# Getting asthma data
+table = myutils.load('asthma_disease_data.csv')
+headers = table.pop(0)
+
+yes = []
+no = []
+
+for i in range(len(table)):
+    if table[i][-1] == "1":
+        yes.append(table[i])
+    else:
+        no.append(table[i])
+    
+myutils.randomize_in_place(no)
+table = yes + no[:900]
+
+myutils.randomize_in_place(table)
+
+# creating a MyPyTable object and loading from file
+print("\nLoading data from asthma_disease_data.csv file")
+asthma_data = mypytable.MyPyTable()
+asthma_data.load_from_file("asthma_disease_data.csv")
+asthma_data.data = [row[:-1] for row in table]  # Removes last column from every row
+asthma_data.column_names = headers[:-1]
+
+# Extract features and target variable as lists
+bmi = list(map(float, asthma_data.get_column('BMI')))  # Convert BMI to float
+bmi = myutils.discretize_bmi(bmi)
+smoking = [1 if x == 'yes' else 0 for x in asthma_data.get_column('Smoking')]  # Encode Smoking
+physical_activity = [1 if x == 'yes' else 0 for x in asthma_data.get_column('PhysicalActivity')]  # Encode PhysicalActivity
+physical_activity = myutils.discretize_physicalactivity(physical_activity)
+lung_function = list(map(float, asthma_data.get_column('LungFunctionFEV1')))  # Convert LungFunction to float
+lung_function = myutils.discretize_lungfunction(lung_function)
+y_data = asthma_data.get_column('Diagnosis')  # Diagnosis is assumed to be numeric
+
+# Combine features into a single list of features
+X_data = list(zip(bmi, smoking, physical_activity, lung_function))  # Now each entry is numeric
+
+# Fitting My Random Forest and getting tree
+my_random_forest = classifier.MyRandomForestClassifier( N=6, M=1, F=4, seed=0)
+my_random_forest.fit(X_data, y_data)
+
+asthma_trees = my_random_forest.trees
+
 # let's package the two lists together into one object
-packaged_obj = [header, interview_tree_solution]
+packaged_obj = [asthma_header, asthma_trees]
 outfile = open("tree.p", "wb")
 pickle.dump(packaged_obj, outfile)
 outfile.close()
